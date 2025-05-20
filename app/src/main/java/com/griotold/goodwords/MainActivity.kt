6266af294d35a1cf9927b81ade4c872916d0cea3
Griotold <gotjd9773@naver.com>
2025-05-20 12:01:53 +0900

    DataBinding - "전체명언보기" 버튼 누르면 SentenceActivity 로 이동

--- app/src/main/java/com/griotold/goodwords/MainActivity.kt
+++ app/src/main/java/com/griotold/goodwords/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.griotold.goodwords
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.griotold.goodwords.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.showAllSentenceBtn.setOnClickListener {
+
+            val intent = Intent(this, SentenceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/griotold/goodwords/SentenceActivity.kt
@@ -0,0 +1,17 @@
+package com.griotold.goodwords
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SentenceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentence)
+
+    }
+}
